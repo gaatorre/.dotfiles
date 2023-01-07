4cdd3dc42fe11a4fa8a8eb8c338efc7dcdd7f0c8
Gabriel Torres <jhd389@gmail.com>
2023-01-07 18:46:07 -0500

    Adding dotfiles
    Signed-off-by: Gabriel Torres <jhd389@gmail.com>

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,26 @@
+alias vim="nvim"
+alias get_idf='. $HOME/dev/esp-idf/export.fish'
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
