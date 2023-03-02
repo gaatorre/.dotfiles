7cfcd1c90a973266b62209464398807bf9bd6193
Gabriel Torres <jhd389@gmail.com>
2023-03-02 00:33:44 -0500

    Updating fish to include hostnames
    Also starting a simple init script to get my env
    up and running.
    
    Signed-off-by: Gabriel Torres <jhd389@gmail.com>

--- omf/.config/omf/init.fish
+++ omf/.config/omf/init.fish
@@ -3,6 +3,6 @@ set -g theme_display_time yes
 
 set -gx fish_prompt_pwd_dir_length 0
 set -g theme_display_group no
-set -g theme_display_hostname no
+set -g theme_display_hostname yes
 set -g theme_color_user aa55ff
 set -g theme_display_rw no
