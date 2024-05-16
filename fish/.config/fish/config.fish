4ce288459086929f95a2a24f2d85c10a83341cf8
Gabriel Torres <gabriel.torres@ll.mit.edu>
2024-05-16 10:32:58 -0400

    Updates
    Signed-off-by: Gabriel Torres <gabriel.torres@ll.mit.edu>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -32,3 +32,11 @@ function fish_user_key_bindings
     bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
     bind -M insert kj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 end
+
+### Setting the right path depending on distro
+switch (uname)
+    case Linux
+        set SHELL "/usr/bin/fish"
+    case Darwin
+        set PM "/opt/homebrew/bin/fish"
+end
